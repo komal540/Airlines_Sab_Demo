bc9a656d17a16157a96a1b56f59c824bd753d595
Komal Vashistha <70580214+komal540@users.noreply.github.com>
2023-10-10 11:36:40 -0700

    Add files via upload

---
+++ src/main/java/com/komal/airlineSab /exception/BookingRestControllerExceptionHandler.java
@@ -0,0 +1,5 @@
+package main.java.com.komal.airlineSab.exception;
+
+public class BookingRestControllerExceptionHandler {
+
+}
---
+++ src/main/java/com/komal/airlineSab /exception/FlightRestControllerExceptionHandler.java
@@ -0,0 +1,10 @@
+package main.java.com.komal.airlineSab.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class FlightRestControllerExceptionHandler {
+	
+	
+
+}
